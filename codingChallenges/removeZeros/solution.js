b5b4529d447c68b66b12c77d85deda4edfaa4ac8
PabloDeLaCruz1 <pablo.de.la.cruz.pro@gmail.com>
2022-04-29 13:29:54 -0400

    problem and solution

---
+++ codingChallenges/removeZeros/solution.js
@@ -0,0 +1,20 @@
+function removeZeros(array) {
+  var temp; // Will hold a temporary value when you move array elements
+  var iterations = array.length; // the 'end' of the array, where zeros start
+  for (var i = 0; i < iterations; i++) {
+    if (array[i] === 0 || array[i] === '0') {
+      for (var j = i; j < array.length - 1; j++) {
+        temp = array[j + 1];
+        array[j + 1] = array[j];
+        array[j] = temp;
+      }
+      i--; // since you shift the entire array backwards
+      iterations--; // marks the spot where the zeros start
+    }
+  }
+  return array;
+}
+
+let myArray = removeZeros([1,0,3,700,05,'0',6,0,7,'0',0]);
+
+console.log(myArray)
